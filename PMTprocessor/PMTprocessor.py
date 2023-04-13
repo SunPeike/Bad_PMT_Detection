90d81ecba6f8290278fe9f6be7badd8bece6f4bc
Peike Sun <peike.sun@kcl.ac.uk>
2023-04-13 02:13:58 +0100

    modified:   PMTprocessor/PMTprocessor.py 	modified:   PMTprocessor/example.ipynb

--- PMTprocessor/PMTprocessor.py
+++ PMTprocessor/PMTprocessor.py
@@ -199,29 +199,87 @@ def first_2_column(folderpath):
     df = relocate_5_columns(folderpath)
     first_2_cols = df.iloc[:, :2]
     return first_2_cols
+def second_2_column(folderpath):
+    df = relocate_5_columns(folderpath)
+    first_2_cols = df.iloc[:, 1:3]
+    return first_2_cols
+def third_2_column(folderpath):
+    df = relocate_5_columns(folderpath)
+    first_2_cols = df.iloc[:, [1,3]]
+    return first_2_cols
+def fourth_2_column(folderpath):
+    df = relocate_5_columns(folderpath)
+    first_2_cols = df.iloc[:, [1,4]]
+    return first_2_cols
 
 
-def generateLabels_neg_main(folderpath):
+def generateLabels(folderpath):
     df = first_2_column(folderpath)
     df_sorted_neg = df.sort_values('qhs_neg', ascending=False)
     df_sorted_main = df.sort_values('qhs_main', ascending=False)
     df['Label'] = -1
-    num_rows = df.shape[0]
-    i = num_rows/10
-    ri = int(round(i))
-    j = num_rows/1000
-    rj = int(round(j))
-    top_100_neg = set(df_sorted_neg.iloc[:rj].index)
+    top_100_neg = set(df_sorted_neg.iloc[:100].index)
     df.loc[top_100_neg, 'Label'] = 1
-    top_100_main = set(df_sorted_main.iloc[:ri].index)
+    top_100_main = set(df_sorted_main.iloc[:100].index)
     df.loc[top_100_main, 'Label'] = 0
     return df
+def second_generateLabels(folderpath):
+    df = second_2_column(folderpath)
+    df_sorted_second = df.sort_values('qhs_second', ascending=False)
+    df_sorted_main = df.sort_values('qhs_main', ascending=False)
+    df['Label'] = -1
+    num_rows = df.shape[0]
+    i = num_rows/1100
+    ri = int(round(i))
+    j = num_rows/5
+    rj = int(round(j))
+    top_100_main = set(df_sorted_main.iloc[:rj].index)
+    df.loc[top_100_main, 'Label'] = 0
+    top_100_second = set(df_sorted_second.iloc[:ri].index)
+    df.loc[top_100_second, 'Label'] = 1
+    return df
 
-def LPA_neg_main(folderpath):
-    df = generateLabels_neg_main(folderpath)
+
+
+
+def third_generateLabels(folderpath):
+    df = third_2_column(folderpath)
+    df_sorted_third = df.sort_values('qhs_third', ascending=False)
+    df_sorted_main = df.sort_values('qhs_main', ascending=False)
+    df['Label'] = -1
+    num_rows = df.shape[0]
+    i = num_rows/1800
+    ri = int(round(i))
+    j = num_rows/5
+    rj = int(round(j))
+    top_100_main = set(df_sorted_main.iloc[:rj].index)
+    df.loc[top_100_main, 'Label'] = 0
+    top_100_third = set(df_sorted_third.iloc[:ri].index)
+    df.loc[top_100_third, 'Label'] = 1
+    return df
+
+def railed_generateLabels(folderpath):
+    df = fourth_2_column(folderpath)
+    df_sorted_railed = df.sort_values('qhs_railed', ascending=False)
+    df_sorted_main = df.sort_values('qhs_main', ascending=False)
+    df['Label'] = -1
+    num_rows = df.shape[0]
+    i = num_rows/725
+    ri = int(round(i))
+    j = num_rows/5
+    rj = int(round(j))
+    top_100_main = set(df_sorted_main.iloc[:rj].index)
+    df.loc[top_100_main, 'Label'] = 0
+    top_100_railed = set(df_sorted_railed.iloc[:ri].index)
+    df.loc[top_100_railed, 'Label'] = 1
+    return df
+
+
+def LPA(folderpath):
+    df = generateLabels(folderpath)
     labeled = df[df['Label'] != -1]
     unlabeled = df[df['Label'] == -1]
-    lp_model = LabelPropagation(kernel='knn', n_neighbors=10, gamma=0.5, max_iter=500)
+    lp_model = LabelPropagation(kernel='knn', n_neighbors=10)
     lp_model.fit(labeled[['qhs_neg', 'qhs_main']], labeled['Label'])
     unlabeled_labels = lp_model.predict(unlabeled[['qhs_neg', 'qhs_main']])
     predicted_labels = pd.concat([labeled[['qhs_neg', 'qhs_main', 'Label']], 
@@ -229,9 +287,112 @@ def LPA_neg_main(folderpath):
                              axis=0)
     predicted_labels = predicted_labels.rename(columns={predicted_labels.columns[-1]: 'pred_label'})
     return predicted_labels
+def LPAsecond(folderpath):
+    df = second_generateLabels(folderpath)
+    labeled = df[df['Label'] != -1]
+    unlabeled = df[df['Label'] == -1]
+    lp_model = LabelPropagation(kernel='knn', n_neighbors=7)
+    lp_model.fit(labeled[['qhs_second', 'qhs_main']], labeled['Label'])
+    unlabeled_labels = lp_model.predict(unlabeled[['qhs_second', 'qhs_main']])
+    predicted_labels = pd.concat([labeled[['qhs_second', 'qhs_main', 'Label']], 
+                              unlabeled[['qhs_second', 'qhs_main']].assign(Label=unlabeled_labels)], 
+                             axis=0)
+    predicted_labels = predicted_labels.rename(columns={predicted_labels.columns[-1]: 'pred_label'})
+    return predicted_labels
+def LPAthird(folderpath):
+    df = third_generateLabels(folderpath)
+    labeled = df[df['Label'] != -1]
+    unlabeled = df[df['Label'] == -1]
+    lp_model = LabelPropagation(kernel='knn', n_neighbors=8)
+    lp_model.fit(labeled[['qhs_third', 'qhs_main']], labeled['Label'])
+    unlabeled_labels = lp_model.predict(unlabeled[['qhs_third', 'qhs_main']])
+    predicted_labels = pd.concat([labeled[['qhs_third', 'qhs_main', 'Label']], 
+                              unlabeled[['qhs_third', 'qhs_main']].assign(Label=unlabeled_labels)], 
+                             axis=0)
+    predicted_labels = predicted_labels.rename(columns={predicted_labels.columns[-1]: 'pred_label'})
+    return predicted_labels
+def LPArailed(folderpath):
+    df = railed_generateLabels(folderpath)
+    labeled = df[df['Label'] != -1]
+    unlabeled = df[df['Label'] == -1]
+    lp_model = LabelPropagation(kernel='knn', n_neighbors=3)
+    lp_model.fit(labeled[['qhs_railed', 'qhs_main']], labeled['Label'])
+    unlabeled_labels = lp_model.predict(unlabeled[['qhs_railed', 'qhs_main']])
+    predicted_labels = pd.concat([labeled[['qhs_railed', 'qhs_main', 'Label']], 
+                              unlabeled[['qhs_railed', 'qhs_main']].assign(Label=unlabeled_labels)], 
+                             axis=0)
+    predicted_labels = predicted_labels.rename(columns={predicted_labels.columns[-1]: 'pred_label'})
+    return predicted_labels
 
 
-def map_label_to_new_col_neg_main(label, pred_label):
+def map_label_to_new_col(label, pred_label):
+    if label == 0:
+        return "set_good"
+    elif label == 1:
+        return "set_thp"
+    elif label == -1:
+        if pred_label == 0:
+            return "pred_good"
+        elif pred_label == 1:
+            return "pred_thp"
+    else:
+        return np.nan
+    
+def create_table_for_drawing(folderpath):
+    beforemodel = generateLabels(folderpath)
+    initiallabel = beforemodel.iloc[:, -1]
+    predicted_labels = LPA(folderpath)
+    
+    allinone = pd.concat([predicted_labels, initiallabel], axis=1)
+    allinone['new_col'] = allinone.apply(lambda row: map_label_to_new_col(row['Label'], row['pred_label']), axis=1)
+    return allinone
+
+
+def second_map_label_to_new_col(label, pred_label):
+    if label == 0:
+        return "set_good"
+    elif label == 1:
+        return "set_qsw"
+    elif label == -1:
+        if pred_label == 0:
+            return "pred_good"
+        elif pred_label == 1:
+            return "pred_qsw"
+    else:
+        return np.nan
+    
+def second_create_table_for_drawing(folderpath):
+    beforemodel = second_generateLabels(folderpath)
+    initiallabel = beforemodel.iloc[:, -1]
+    predicted_labels = LPAsecond(folderpath)
+    
+    allinone = pd.concat([predicted_labels, initiallabel], axis=1)
+    allinone['new_col'] = allinone.apply(lambda row: second_map_label_to_new_col(row['Label'], row['pred_label']), axis=1)
+    return allinone
+
+def third_map_label_to_new_col(label, pred_label):
+    if label == 0:
+        return "set_good"
+    elif label == 1:
+        return "set_dp"
+    elif label == -1:
+        if pred_label == 0:
+            return "pred_good"
+        elif pred_label == 1:
+            return "pred_dp"
+    else:
+        return np.nan
+    
+def third_create_table_for_drawing(folderpath):
+    beforemodel = third_generateLabels(folderpath)
+    initiallabel = beforemodel.iloc[:, -1]
+    predicted_labels = LPAthird(folderpath)
+    
+    allinone = pd.concat([predicted_labels, initiallabel], axis=1)
+    allinone['new_col'] = allinone.apply(lambda row: third_map_label_to_new_col(row['Label'], row['pred_label']), axis=1)
+    return allinone
+
+def railed_map_label_to_new_col(label, pred_label):
     if label == 0:
         return "set_good"
     elif label == 1:
@@ -243,21 +404,23 @@ def map_label_to_new_col_neg_main(label, pred_label):
             return "pred_railed"
     else:
         return np.nan
-
-def create_table_for_drawing_neg_main(folderpath):
-    beforemodel = generateLabels_neg_main(folderpath)
+    
+def railed_create_table_for_drawing(folderpath):
+    beforemodel = railed_generateLabels(folderpath)
     initiallabel = beforemodel.iloc[:, -1]
-    predicted_labels = LPA_neg_main(folderpath)
+    predicted_labels = LPArailed(folderpath)
+    
     allinone = pd.concat([predicted_labels, initiallabel], axis=1)
-    allinone['new_col'] = allinone.apply(lambda row: map_label_to_new_col_neg_main(row['Label'], row['pred_label']), axis=1)
+    allinone['new_col'] = allinone.apply(lambda row: railed_map_label_to_new_col(row['Label'], row['pred_label']), axis=1)
     return allinone
 
 
-def graphLPA_neg_main(folderpath):
-    allinone = create_table_for_drawing_neg_main(folderpath)
+################# drawing lpa ###########################
+def graphLPA(folderpath):
+    allinone = create_table_for_drawing(folderpath)
     # create a dictionary to map new_col values to colors and shapes
     color_dict = {"set_good": 'royalblue', "set_railed": 'darkorange', "pred_good": 'royalblue', "pred_railed": 'darkorange'}
-    shape_dict = {"set_good": 'o', "set_railed": 'o', "pred_good": 'x', "pred_railed": 'x'}
+    shape_dict = {"set_good": 's', "set_railed": 's', "pred_good": 'o', "pred_railed": 'o'}
 
 
 # create a scatter plot
@@ -283,7 +446,130 @@ def graphLPA_neg_main(folderpath):
 # show the plot
     plt.show()
     
- ############################ LPA for all columns#################
+    
+def second_graphLPA(folderpath):
+    allinone = second_create_table_for_drawing(folderpath)
+    # create a dictionary to map new_col values to colors and shapes
+    color_dict = {"set_good": 'royalblue', "set_qsw": 'darkorange', "pred_good": 'royalblue', "pred_qsw": 'darkorange'}
+    shape_dict = {"set_good": 'o', "set_qsw": 'o', "pred_good": 'x', "pred_qsw": 'x'}
+
+# create a scatter plot
+    fig, ax = plt.subplots()
+    for i, row in allinone.iterrows():
+        ax.scatter(row['qhs_main'], row['qhs_second'], c=color_dict[row['new_col']], marker=shape_dict[row['new_col']])
+
+# add legend
+    handles = []
+    labels = []
+    for new_col, color in color_dict.items():
+        shape = shape_dict[new_col]
+        label = f'{new_col}'
+        handles.append(ax.scatter([], [], c=color, marker=shape))
+        labels.append(label)
+    ax.legend(handles, labels, loc='best', title='Legend')
+
+# add labels and title
+    ax.set_xlabel('qhs_main')
+    ax.set_ylabel('qhs_second')
+    ax.set_title('LPA for qhs_second and qhs_main, n_neighbors = 7')
+
+# show the plot
+    plt.show() 
+    
+def third_graphLPA(folderpath):
+    allinone = third_create_table_for_drawing(folderpath)
+    # create a dictionary to map new_col values to colors and shapes
+    color_dict = {"set_good": 'royalblue', "set_dp": 'darkorange', "pred_good": 'royalblue', "pred_dp" :'darkorange'}
+    shape_dict = {"set_good": 'o', "set_dp" :'o', "pred_good": 'x', "pred_dp":'x'}
+
+# create a scatter plot
+    fig, ax = plt.subplots()
+    for i, row in allinone.iterrows():
+        ax.scatter(row['qhs_main'], row['qhs_third'], c=color_dict[row['new_col']], marker=shape_dict[row['new_col']])
+
+# add legend
+    handles = []
+    labels = []
+    for new_col, color in color_dict.items():
+        shape = shape_dict[new_col]
+        label = f'{new_col}'
+        handles.append(ax.scatter([], [], c=color, marker=shape))
+        labels.append(label)
+    ax.legend(handles, labels, loc='best', title='Legend')
+
+# add labels and title
+    ax.set_xlabel('qhs_main')
+    ax.set_ylabel('qhs_third')
+    ax.set_title('LPA for qhs_third and qhs_main, n_neighbors = 8')
+
+# show the plot
+    plt.show()   
+    
+    
+def railed_graphLPA(folderpath):
+    allinone = railed_create_table_for_drawing(folderpath)
+    # create a dictionary to map new_col values to colors and shapes
+    color_dict = {"set_good": 'royalblue', "set_railed": 'darkorange', "pred_good": 'royalblue', "pred_railed" :'darkorange'}
+    shape_dict = {"set_good": 'o', "set_railed" :'o', "pred_good": 'x', "pred_railed":'x'}
+
+# create a scatter plot
+    fig, ax = plt.subplots()
+    for i, row in allinone.iterrows():
+        ax.scatter(row['qhs_main'], row['qhs_railed'], c=color_dict[row['new_col']], marker=shape_dict[row['new_col']])
+
+# add legend
+    handles = []
+    labels = []
+    for new_col, color in color_dict.items():
+        shape = shape_dict[new_col]
+        label = f'{new_col}'
+        handles.append(ax.scatter([], [], c=color, marker=shape))
+        labels.append(label)
+    ax.legend(handles, labels, loc='best', title='Legend')
+
+# add labels and title
+    ax.set_xlabel('qhs_main')
+    ax.set_ylabel('qhs_railed')
+    ax.set_title('LPA for qhs_railed and qhs_main, n_neighbors = 3')
+
+# show the plot
+    plt.show()  
+    
+    
+############# output bad channels and count #############
+def bad_channels(filepath):
+    df1 = create_table_for_drawing(filepath)
+    df2 = second_create_table_for_drawing(filepath)
+    df3 = third_create_table_for_drawing(filepath)
+    df4 = railed_create_table_for_drawing(filepath)
+    
+    df1_sorted = df1.sort_index()["pred_label"]
+    df2_sorted = df2.sort_index()["pred_label"]
+    df3_sorted = df3.sort_index()["pred_label"]
+    df4_sorted = df4.sort_index()["pred_label"]
+    addup = df2_sorted.add(df1_sorted).add(df3_sorted).add(df4_sorted)
+    df = pd.DataFrame(addup)
+    ones_df = df[df['pred_label'] == 1]
+    ones_index = ones_df.index.tolist()
+    num_good = df_concat['type'].value_counts()['good']
+    return(ones_index)
+
+def bad_channels_count(filepath):
+    df1 = create_table_for_drawing(filepath)
+    df2 = second_create_table_for_drawing(filepath)
+    df3 = third_create_table_for_drawing(filepath)
+    df4 = railed_create_table_for_drawing(filepath)
+    
+    df1_sorted = df1.sort_index()["pred_label"]
+    df2_sorted = df2.sort_index()["pred_label"]
+    df3_sorted = df3.sort_index()["pred_label"]
+    df4_sorted = df4.sort_index()["pred_label"]
+    addup = df2_sorted.add(df1_sorted).add(df3_sorted).add(df4_sorted)
+    df = pd.DataFrame(addup)
+    num_good = df_concat['type'].value_counts()['bad']
+    return(num_good)
+
+############################ LPA for all columns#################
 
 def generateLabelsall_v2(folderpath):
     df = relocate_5_columns(folderpath)
